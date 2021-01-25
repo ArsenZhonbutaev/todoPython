b9a9768b20dd232bba45e6358dea0e2b044c659b
ArsenZhonbutaev <zhonbutaev.arsen@gmail.com>
2021-01-25 19:45:32 +0600

    added marking

--- main/views.py
+++ main/views.py
@@ -1,22 +1,42 @@
+  
 from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo
 
+
 def homepage(request):
     return render(request, "index.html")
+
 
 def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
 
-def second(request):
-    return HttpResponse("test 2")
 
-def third(request):
-    return HttpResponse("this is third test page")
+def second(request):
+    return HttpResponse("test 2 page")
+
 
 def add_todo(request):
     form = request.POST
-    text = form['todo_text']
+    text = form["todo_text"]
     todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
+
+
+def delete_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.delete()
+    return redirect(test)
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test)
+
+def unmark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = False
     todo.save()
     return redirect(test)
